bf348bd5ee2a90a7deb7e69f22963c231be3436b
anna-zukowska <anna-zukowska@ya.ru>
2022-12-15 20:43:33 +0500

    changed model

--- wikipedia_tests/model/app.py
+++ wikipedia_tests/model/app.py
@@ -1,9 +1,12 @@
 from .pages.main_page import MainPage
 from .pages.settings_page import SettingsPage
+from .pages.search_results_page import SearchResultsPage
 
 
 main_page = MainPage()
 settings_page = SettingsPage()
+search_results_page = SearchResultsPage()
+
 
 
 
--- wikipedia_tests/model/pages/search_results_page.py
+++ wikipedia_tests/model/pages/search_results_page.py
@@ -1,32 +1,18 @@
 import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from selene.support.shared import browser
-from selene.support.conditions import have, be
+from selene.support.conditions import have
 
 
-class SettingsPage():
-    title_settings = (AppiumBy.CLASS_NAME, 'android.widget.TextView')
+class SearchResultsPage():
     button_return = (AppiumBy.CLASS_NAME, 'android.widget.ImageButton')
-
-    def assert_open(self):
-        with allure.step('Проверяем, что открылась страница Settings'):
-            browser.all(self.title_settings).element_by(have.text('Settings')).should(be.visible)
+    search_results = (AppiumBy.CLASS_NAME, 'android.widget.TextView')
 
     def button_return_click(self):
         with allure.step('Нажимаем кнопку возврата на предыдущую страницу'):
             browser.element(self.button_return).click()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def assert_results_exist(self):
+        with allure.step('Выполняем проверку результатов поиска'):
+            browser.all(self.search_results).should(have.size_greater_than(0))
 
